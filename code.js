432aa66d450765d535df2aa257877fffbe462cef
Alex Turbin <turbin28@gmail.com>
2022-02-12 23:44:57 +0300

    Clocky Mc Clock-Face

--- code.js
+++ code.js
@@ -1,25 +1,17 @@
-// Bit Counting
-// https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
 
-// assert.strictEqual(countBits(10), 2);
-
-function countBits(n) {
-  let result = 0;
-  let bit  = n.toString(2)
-  Array.from(bit).forEach((element) => {
-    if (element === "1") {
-      result++;
+var whatTimeIsIt = function (angle) {
+    let h = Math.floor(angle / 30);
+    let m = Math.trunc((60 / 30) * (angle % 30));
+    if (h === 0) {
+      h = 12;
     }
-  });
-  return result;
-}
+    return `${zero(h)}${h}:${zero(m)}${m}`;
+  };
+  
+  function zero(x) {
+    return x < 10 ? "0" : "";
+  }
 
-console.log(countBits(6905847168)); // 1
-
-countBits(6905847168) //should equal 18 expected 17 to equal 18
-
-console.log((6905847168 >>> 0).toString(2))
-
-
-10011011100111101101110110000000
-110011011100111101101110110000000
+console.log(whatTimeIsIt(90));
+console.log(whatTimeIsIt(40)); //01.20
+console.log(whatTimeIsIt(339.3473072102758)); //339.3473072102758 = 11:18
