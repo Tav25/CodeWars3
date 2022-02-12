4a3a13bbfffbba1f38b4d92a9869d30b4f9f91f7
Alex Turbin <turbin28@gmail.com>
2022-02-13 02:08:18 +0300

    Lottery Ticket

--- code.js
+++ code.js
@@ -1,17 +1,40 @@
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    2
+  )
+); //, 'Loser!');
+console.log(
+  bingo(
+    [
+      ["ABC", 65],
+      ["HGR", 74],
+      ["BYHT", 74],
+    ],
+    1
+  )
+); //, 'Winner!');
+console.log(
+  bingo(
+    [
+      ["HGTYRE", 74],
+      ["BE", 66],
+      ["JKTY", 74],
+    ],
+    3
+  )
+); //, 'Loser!');
 
-var whatTimeIsIt = function (angle) {
-    let h = Math.floor(angle / 30);
-    let m = Math.trunc((60 / 30) * (angle % 30));
-    if (h === 0) {
-      h = 12;
-    }
-    return `${zero(h)}${h}:${zero(m)}${m}`;
-  };
-  
-  function zero(x) {
-    return x < 10 ? "0" : "";
-  }
+// console.log("ABC".charCodeAt(1));
 
-console.log(whatTimeIsIt(90));
-console.log(whatTimeIsIt(40)); //01.20
-console.log(whatTimeIsIt(339.3473072102758)); //339.3473072102758 = 11:18
+function bingo(ticket, win) {
+  let result = 0;
+  ticket.forEach((element) => {
+    element[0].includes(String.fromCharCode(element[1])) ? result++ : null;
+  });
+    return result>= win ? "Winner!":"Loser!";
+}
